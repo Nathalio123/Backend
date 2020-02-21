cf563674a4870cc3db6bfe7e45a9e938d3088649
Joberth Rogers <joberth.rogers18@gmail.com>
2020-02-21 16:37:02 -0400

    Added cors
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const routes = require('./routes');
+const cors = require('cors');
 const database = require('./services/database');
 
 class App {
@@ -13,6 +14,7 @@ class App {
   }
 
   middlewares() {
+    this.use(cors());
     this.express.use(express.json());
   }
 
